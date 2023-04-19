37cab78970dc3ddfa9245a0189f613b7d54d1ddf
Neelesh Aggarwal <86107529+AggarwalNeelesh@users.noreply.github.com>
2023-04-19 21:13:00 +0530

    second

---
+++ data.js
@@ -0,0 +1,12 @@
+const data = {
+   a: {name: "Neelesh",
+    age: 22,
+    Motivation: "Learning"},
+    b: {name: "Neelesh",
+    age: 22,
+    Motivation: "Learning"},
+    c: {name: "Neelesh",
+    age: 22,
+    Motivation: "Learning"},
+}
+module.exports = data;
