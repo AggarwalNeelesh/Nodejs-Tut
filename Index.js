d0d422f33b3b175bcb356a352f1481f3f943250c
Neelesh Aggarwal <86107529+AggarwalNeelesh@users.noreply.github.com>
2023-04-19 21:19:26 +0530

    Creating and deleting files

--- Index.js
+++ Index.js
@@ -1,7 +1,21 @@
 const http = require('http');
-const data = require('./data')
-http.createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(5000);
+const path = require('path');
+const fs = require('fs');
+const dirpath = path.join(__dirname, 'files');
+
+// Creatint files
+for(var i=0;i<10;i++){
+    fs.writeFileSync(`${dirpath}/hello${i}.txt`, `Hello I am file number ${i}`);
+}
+
+// Getting Files list
+fs.readdir(dirpath, (err, files)=>{
+    files.forEach((item)=>{
+        console.log(item);
+    })
+})
+
+// Deleting Files
+for(var i=0;i<10;i++){
+    fs.unlinkSync(`${dirpath}/hello${i}.txt`);
+}
