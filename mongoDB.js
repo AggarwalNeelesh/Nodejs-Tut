1737a055533c4a44f372e5a81996610c420a88a6
Neelesh Aggarwal <86107529+AggarwalNeelesh@users.noreply.github.com>
2023-05-20 20:09:20 +0530

    MongoDB Connections Established

--- mongoDB.js
+++ mongoDB.js
@@ -2,13 +2,11 @@ const {MongoClient,ServerApiVersion} = require('mongodb');
 
 const url = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(url);
-// console.log(client);
+
 async function getData(){
-    console.log("sdsd");
+    // connect() returns a promise so to handle it we use Await, for using await function should be async
     const result = await client.connect();
     const db = await result.db('e-comm');
-    // console.log(1,db);
-    console.log("efd");
     const collection = db.collection('products');
     const response = await collection.find().toArray();
     console.log(response);
